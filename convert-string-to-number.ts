107a09c4940d84fd322ce2324fff98d991a85dd8
Qobil7337 <qobil7337@mail.ru>
2025-05-08 18:18:28 +0500

    convert string to number

---
+++ convert-string-to-number.ts
@@ -0,0 +1,22 @@
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+//
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+function stringToNumber(str: string): number {
+    return Number(str);
+}
+const result = stringToNumber('-4');
+console.log(result);
+
+// here are some variations
+// const stringToNumber = (str: string): number => +str;
+//
+// function stringToNumber(str: string): number {
+//     return parseInt(str);
+// }
